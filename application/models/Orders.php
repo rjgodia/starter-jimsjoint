21bcda146d485a9a4b208974787c38bf6c9372a1
brandon <khoitran00@gmail.com>
2016-02-16 18:23:16 -0800

    Handle the checkout process

--- application/models/Orders.php
+++ application/models/Orders.php
@@ -63,7 +63,18 @@ class Orders extends MY_Model {
     // validate an order
     // it must have at least one item from each category
     function validate($num) {
-        return false;
+
+        $CI =& get_instance();
+        $items = $CI->orderitems->group($num);
+        $gotem = array();
+
+        foreach($items as $item)
+        {
+            $menu = $CI->menu->get($item->item);
+            $gotem[$menu->category] = 1;
+        }
+
+        return isset($gotem['m']) && isset($gotem['d']) && isset($gotem['s']);
     }
 
 }
